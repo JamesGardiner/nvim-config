c0a4b4318da08c83af554196d50938ea521f1e9d
“James <james@jgardiner.co.uk>
2022-10-29 23:29:49 +0100

    feat: octo plugin config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -393,18 +393,6 @@ packer.startup {
       "folke/trouble.nvim",
       requires = "kyazdani42/nvim-web-devicons",
     }
-
-    use {
-      "pwntester/octo.nvim",
-      requires = {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
-        "kyazdani42/nvim-web-devicons",
-      },
-      config = function()
-        require("octo").setup()
-      end,
-    }
   end,
   config = {
     max_jobs = 16,
