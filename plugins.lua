01c690d992d077c5d78eb217364fb22ff731c5ed
James Gardiner <james@jgardiner.co.uk>
2024-02-07 11:14:53 +0000

    Add vim-fugitive

--- plugins.lua
+++ plugins.lua
@@ -59,6 +59,11 @@ local plugins = {
   },
 
   {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
+
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
